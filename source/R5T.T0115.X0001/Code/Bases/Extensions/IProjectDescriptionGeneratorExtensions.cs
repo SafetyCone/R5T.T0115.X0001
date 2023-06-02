02079e77502cdb201480711a61fb24b9c7893916
davidcoats <david@getsafetycone.com>
2023-06-01 18:07:23 -0700

    Interim changes.

--- source/R5T.T0115.X0001/Code/Bases/Extensions/IProjectDescriptionGeneratorExtensions.cs
+++ source/R5T.T0115.X0001/Code/Bases/Extensions/IProjectDescriptionGeneratorExtensions.cs
@@ -7,6 +7,7 @@ namespace System
 {
     public static class IProjectDescriptionGeneratorExtensions
     {
+        [Obsolete("See R5T.O0007.IProjectDescriptionOperations.Get_ConstructionProjectDescription()")]
         public static string GetConstructionProjectDescription(this IProjectDescriptionGenerator _,
            string libraryDescription)
         {
